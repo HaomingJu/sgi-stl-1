0ba5f3cbbdd62567a535df9a1b9f667ce8521a7d
HaomingJu <juhaoming@trunk.tech>
2021-07-08 17:39:14 +0800

    ag -w __STL_STATIC_TEMPLATE_MEMBER_BUG -l | xargs -I @ sunifdef -U __STL_STATIC_TEMPLATE_MEMBER_BUG  @ -r

--- alloc.h
+++ alloc.h
@@ -30,9 +30,6 @@ using __STD::debug_alloc;
 using __STD::__default_alloc_template; 
 using __STD::alloc; 
 using __STD::single_client_alloc; 
-#ifdef __STL_STATIC_TEMPLATE_MEMBER_BUG
-using __STD::__malloc_alloc_oom_handler; 
-#endif /* __STL_STATIC_TEMPLATE_MEMBER_BUG */
 #ifdef __STL_USE_STD_ALLOCATORS 
 using __STD::allocator;
 #endif /* __STL_USE_STD_ALLOCATORS */
